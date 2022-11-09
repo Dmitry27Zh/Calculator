eb613052516063e1e22676b3dd12b570f71ae07d
Dmitry <dmitry27zh@gmail.com>
2022-11-09 20:28:33 +0500

    Add demo items

--- js/index.js
+++ js/index.js
@@ -1,6 +1,33 @@
 import { Calculator } from './modules/calculator.js'
 
-const container = document.querySelector('.container')
 const element = document.querySelector('.calculator')
+const dynamicContainer = document.querySelector('#dynamic-container')
+const dynamicBtn = document.querySelector('#dynamic-btn')
+const dynamicExternalBtn = document.querySelector('#dynamic-external-btn')
+const dynamicExtrenalContainer = document.querySelector('#dynamic-external-container')
+const dynamicExternalExpression = document.querySelector('#dynamic-external-expression')
+const dynamicExternalResult = document.querySelector('#dynamic-external-result')
 
-const staticCalculator = new Calculator(element, { resultOutput: result })
+const staticCalculator = new Calculator(element)
+
+dynamicBtn.addEventListener(
+  'click',
+  () => {
+    const dynamicCalculator = new Calculator(null, null, { container: dynamicContainer })
+  },
+  { once: true }
+)
+
+dynamicExternalBtn.addEventListener(
+  'click',
+  () => {
+    const dynamicExternalCalculator = new Calculator(
+      null,
+      { expressionOutput: dynamicExternalExpression, resultOutput: dynamicExternalResult },
+      { container: dynamicExtrenalContainer }
+    )
+  },
+  {
+    once: true,
+  }
+)
